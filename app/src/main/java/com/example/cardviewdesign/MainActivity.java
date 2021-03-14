e1d4d82b13c054c572e2614f39f344e11fcb65e9
Angel_Antolin <fractalangelantolin@gmail.com>
2021-03-13 22:06:24 -0600

    mi primer commit

---
+++ app/src/main/java/com/example/cardviewdesign/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.cardviewdesign;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView v1, v2, v3, v4, v5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        v1 = findViewById(R.id.language);
+        v1.setImageResource(R.drawable.ic_language);
+
+        v2 = findViewById(R.id.widgets);
+        v2.setImageResource(R.drawable.ic_widgets);
+
+        v3 = findViewById(R.id.charla);
+        v3.setImageResource(R.drawable.ic_baseline_chat_24);
+
+        v4 = findViewById(R.id.calendar);
+        v4.setImageResource(R.drawable.ic_calendar);
+
+        v5 = findViewById(R.id.informe);
+        v5.setImageResource(R.drawable.ic_info);
+
+    }
+
+
+
+
+}
